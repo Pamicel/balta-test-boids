69939dc24224de61f0fb952792a8ed7a73d4c4ba
Pamicel <paulamicel@gmail.com>
2023-11-27 01:54:10 +0100

    move reynolds algo to flock object

--- Flock.ts
+++ Flock.ts
@@ -1,4 +1,6 @@
+import * as THREE from "three";
 import { Boid } from "./Boid";
+import { settings } from "./config";
 
 export class Flock {
   public boids: Boid[];
@@ -27,7 +29,79 @@ export class Flock {
   }
 
   public update(): void {
+    // Implement Reynolds' Boid algorithm behaviors
     for (const boid of this.boids) {
+      const separationForce: THREE.Vector3 = new THREE.Vector3();
+      const alignmentForce: THREE.Vector3 = new THREE.Vector3();
+      const cohesionForce: THREE.Vector3 = new THREE.Vector3();
+      let separationCount: number = 0;
+      let alignmentCount: number = 0;
+      let cohesionCount: number = 0;
+
+      for (const otherBoid of this.boids) {
+        if (otherBoid !== boid) {
+          const distance: number = boid.position.distanceTo(otherBoid.position);
+
+          // Separation
+          if (distance < settings.separationRadius) {
+            // if distance is zero. push the boid in a random direction
+            if (distance !== 0) {
+              const diff: THREE.Vector3 = new THREE.Vector3().subVectors(
+                boid.position,
+                otherBoid.position
+              );
+              diff.normalize().divideScalar(distance);
+              separationForce.add(diff);
+            } else {
+              const randomDirection: THREE.Vector3 = new THREE.Vector3(
+                Math.random() * 2 - 1,
+                Math.random() * 2 - 1,
+                Math.random() * 2 - 1
+              );
+              separationForce.add(randomDirection);
+            }
+            separationCount++;
+          }
+
+          // Alignment
+          if (distance < settings.alignmentRadius) {
+            alignmentForce.add(otherBoid.velocity);
+            alignmentCount++;
+          }
+
+          // Cohesion
+          if (distance < settings.cohesionRadius) {
+            cohesionForce.add(otherBoid.position);
+            cohesionCount++;
+          }
+        }
+      }
+
+      // Apply separation, alignment, and cohesion forces
+      if (separationCount > 0) {
+        separationForce.divideScalar(separationCount);
+        separationForce.normalize();
+        separationForce.multiplyScalar(settings.separationFactor); // Adjust separation strength
+      }
+
+      if (alignmentCount > 0) {
+        alignmentForce.divideScalar(alignmentCount);
+        alignmentForce.normalize();
+        alignmentForce.multiplyScalar(settings.alignmentFactor); // Adjust alignment strength
+      }
+
+      if (cohesionCount > 0) {
+        cohesionForce.divideScalar(cohesionCount);
+        cohesionForce.sub(boid.position);
+        cohesionForce.normalize();
+        cohesionForce.multiplyScalar(settings.cohesionFactor); // Adjust cohesion strength
+      }
+
+      // Update boid's velocity based on forces
+      boid.applyForce(separationForce);
+      boid.applyForce(alignmentForce);
+      boid.applyForce(cohesionForce);
+      // Update boid's position based on velocity
       boid.update();
     }
   }
